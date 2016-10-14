9dc8458ccf9e0c6f4deb68aa63db9e6e88e3a41b
Rhishikesh Limaye <rhishi@gmail.com>
2016-10-13 17:09:00 -0700

    WPF app snippet demo'ing TextBlock with less/more button mechanism, and first-last-age-email DataGrid.

---
+++ LessMoreAndDataGridDemo/MainWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Data;
+
+namespace LessMoreAndDataGridDemo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            var personInfos = new[]
+            {
+                new { FirstName = "Lorem", LastName = "Ipsum", Age = 33, Email = new Uri("mailto:lorem34s@gmail.com") },
+                new { FirstName = "Labore", LastName = "Aliqua", Age = 35, Email = new Uri("mailto:labore.aliqua@gmail.com") },
+                new { FirstName = "Aute", LastName = "Cillum", Age = 34, Email = new Uri("mailto:autecillum@gmail.com") },
+                new { FirstName = "Veniam", LastName = "Proident", Age = 38, Email = new Uri("mailto:sedveniam@gmail.com") },
+                new { FirstName = "Officia", LastName = "Adipiscing", Age = 32, Email = new Uri("mailto:offiscing@gmail.com") },
+                new { FirstName = "Mollit", LastName = "Irure", Age = 30, Email = new Uri("mailto:iruremollit@gmail.com") },
+                new { FirstName = "Amet", LastName = "Cupidatat", Age = 30, Email = new Uri("mailto:cupidatatamet@gmail.com") },
+                new { FirstName = "Consec", LastName = "Tetur", Age = 30, Email = new Uri("mailto:consectetur@gmail.com") }
+            };
+
+            Grid1.ItemsSource = personInfos;
+            Grid2.DataContext = personInfos;
+            Grid3.DataContext = personInfos;
+        }
+
+        private void LessMoreButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            ((MainWindowViewModel)this.DataContext).ToggleLessMore();
+        }
+    }
+
+    /// <summary>
+    /// Converts between "mailto:" URI and string that is just the email address.
+    /// </summary>
+    public class EmailConverter : IValueConverter
+    {
+        /// <summary>
+        /// Converts from "mailto:" URI to string email address.
+        /// </summary>
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (value != null)
+            {
+                var emailUri = value.ToString();
+                var email = emailUri.Substring(7, emailUri.Length - 7);
+                return email;
+            }
+            return String.Empty;
+        }
+
+        /// <summary>
+        /// Converts string email address to "mailto:" URI.
+        /// </summary>
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            var email = new Uri((string)value);
+            return email;
+        }
+    }
+}
---
+++ LessMoreAndDataGridDemo/MainWindowViewModel.cs
@@ -0,0 +1,53 @@
+﻿using System.ComponentModel;
+
+namespace LessMoreAndDataGridDemo
+{
+    public class MainWindowViewModel : INotifyPropertyChanged
+    {
+        /// <summary>
+        /// Lorem Ipsum text taken from www.lipsum.com and Wikipedia
+        /// </summary>
+        public const string LoremIpsum =
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do " +
+            "eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim " +
+            "ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut " +
+            "aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit " +
+            "in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur " +
+            "sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt " +
+            "mollit anim id est laborum.";
+
+        public MainWindowViewModel()
+        {
+            Text = LoremIpsum;
+            ShowMore = true;
+        }
+
+        public string Text
+        {
+            get;
+            private set;
+        }
+
+        public string LessMore => ShowMore ? "less" : "more";
+
+        public bool ShowMore
+        {
+            get;
+            private set;
+        }
+
+        public void ToggleLessMore()
+        {
+            ShowMore = !ShowMore;
+            NotifyPropertyChanged("ShowMore");
+            NotifyPropertyChanged("LessMore");
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
